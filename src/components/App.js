a7d433667e49f5345ce256b5f6055258437df97e
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-28 01:56:22 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">Inventory Management of Project</h1>
+        <p data-ns-test="project-description">
+          For College Students to keep track of project data
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
