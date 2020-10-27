8b709f685586289f86d193b8935fb1c1cd3d5acd
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-28 02:00:00 +0530

    try2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <h1 data-ns-test="project-name">Inventory Management of Project</h1>
+        <h1>
+          <p data-ns-test="project-name">Inventory Management of Project </p>
+        </h1>
         <p data-ns-test="project-description">
           For College Students to keep track of project data
         </p>
